755156fc498280a4d4420fc28102136ae4f91e15
Vishal Chauhan <vishalmessi550@gmail.com>
2024-11-16 16:22:41 +0530

    .

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import {createContext } from "react";
+import {createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 
 export const ShopContext = createContext()
@@ -6,11 +6,40 @@ export const ShopContext = createContext()
 const ShopContextProvider = (props) => {
     const currency = '$';
     const deliveryFee = 10;
+    const [search, setSearch] = useState('')
+    const [showSearch, setShowSearch] = useState(false)
+    const [cartItems, setCartItems] = useState({})
+
+    const addToCart = async (itemId, size) => {
+        let cartData = structuredClone(cartItems)
+
+        if (cartData[itemId]) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1
+            } else {
+                cartData[itemId][size] = 1
+            }
+        } else {
+            cartData[itemId] = {}
+            cartData[itemId][size] = 1
+        }
+        setCartItems(cartData)
+    }
+
+    useEffect(() => {
+        console.log(cartItems)
+    }, [cartItems])
 
     const value = {
         products,
         currency,
-        deliveryFee
+        deliveryFee,
+        search,
+        setSearch,
+        showSearch,
+        setShowSearch,
+        cartItems, 
+        addToCart
     }
 
     return (
