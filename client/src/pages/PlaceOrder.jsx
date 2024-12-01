d2442ae391fecfb9ea764f68071c9424b5072925
Vishal Chauhan <vishalmessi550@gmail.com>
2024-12-01 14:59:02 +0530

    fixed minor bugs

--- client/src/pages/PlaceOrder.jsx
+++ client/src/pages/PlaceOrder.jsx
@@ -122,7 +122,8 @@ const PlaceOrder = () => {
               <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-green-400' : ''}`}></p>
               <img className='h-5 mx-4' src={assets.stripe_logo} alt="" />
             </div>
-            <div onClick={() => setMethod('razorpay')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+            {/* onClick={() => setMethod('razorpay')} -> removed temporarily until razor confirmation */}
+            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer grayscale bg-neutral-100'>
               <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'razorpay' ? 'bg-green-400' : ''}`}></p>
               <img className='h-5 mx-4' src={assets.razorpay_logo} alt="" />
             </div>
